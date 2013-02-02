672ed43bcdb531ac30bcf4aef841a3c071038f71
josep <jbatalle3@gmail.com>
2013-02-02 21:00:19 +0000

    Version 1.2

--- main.js
+++ main.js
@@ -1,27 +1,26 @@
-/*#########################################################################
-#                                                                         #
-#   This Script allows you to listen the music allocated in google        #
-#   music account using Amarok                                            #
-#                                                                         #
-#   Copyright                                                             #
-#                                                                         #
-#   (C)  2013 Josep Batalle Oronich <jbatalle3@gmail.com>                 #
-#                                                                         #
-#   This program is free software; you can redistribute it and/or modify  #
-#   it under the terms of the GNU General Public License as published by  #
-#   the Free Software Foundation; either version 2 of the License, or     #
-#   (at your option) any later version.                                   #
-#                                                                         #
-#   This program is distributed in the hope that it will be useful,       #
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of        #
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
-#   GNU General Public License for more details.                          #
-#                                                                         #
-#   You should have received a copy of the GNU General Public License     #
-#   along with this program; if not, write to the                         #
-#   Free Software Foundation, Inc.,                                       #
-#   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.         #
-##########################################################################*/
+/*
+ *		This Script allows you to listen the music allocated in google
+ *		music account using Amarok
+ *
+ * Copyright (C) 
+ *
+ *  (C)  2013 Josep Batalle Oronich <jbatalle3@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Version: 1.2
+ */
 
 Importer.loadQtBinding("qt.core");
 Importer.loadQtBinding("qt.gui");
